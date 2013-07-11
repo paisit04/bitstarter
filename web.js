1e7188ba88599f398e4cd4305afad6c894191bbe
Paisit Jarunnamsiri <paisit.j@gmail.com>
2013-07-11 06:07:57 +0000

    read from index.html

--- web.js
+++ web.js
@@ -4,13 +4,10 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buf = new Buffer(1024);
   fs.readFile('index.html', function (err, data) {
     if (err) throw err;
-    buf.write(data);
+    response.send(data.toString("utf-8"));
   });
-
-  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
