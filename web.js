0cb42cdb9cb589e7126a201131d8dc0b3d1a6dc0
Paisit Jarunnamsiri <paisit.j@gmail.com>
2013-07-11 05:51:29 +0000

    read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buf = new Buffer(1024);
+  fs.readFile('index.html', function (err, data) {
+    if (err) throw err;
+    buf.write(data);
+  });
+
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
